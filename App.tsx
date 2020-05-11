f5d17e66a68b1d8328c33a0a37d2a61ab268c7ef
Martin Villalba <14tinchov@gmail.com>
2020-05-11 09:03:33 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  return <WebView source={{ uri: 'https://quierounturno-staging.herokuapp.com/companies/sign_in' }} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
