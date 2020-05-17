1e54ba72a3f7bf67d0a8f5091bdaaafcb1b40aa3
Martin Villalba <14tinchov@gmail.com>
2020-05-17 16:03:50 -0300

    - Fix share and download PDF

--- App.tsx
+++ App.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
+import { Linking, Alert, Share } from 'react-native';
 
-export default function App() {
-  return <WebView source={{ uri: 'https://www.quierounturno.com//companies/sign_in' }} />;
+const onShare = async (message: any) => {
+  try {
+    const result = await Share.share({ message: message });
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+export default class App extends React.Component {
+  webview: any;
+
+  render() {
+    return (
+      <WebView
+        ref={(ref) => { this.webview = ref; }}
+        source={{ uri: 'https://www.quierounturno.com/companies/sign_in' }}
+        onMessage={(event) => { onShare(event.nativeEvent.data) } }
+        onNavigationStateChange={(event) => {
+          var splitUrl = event.url.split(".");
+          var isPDF = splitUrl.slice(-1)[0]
+          if (isPDF === "pdf") {
+            this.webview.stopLoading();
+            Linking.openURL(event.url);
+          }
+        }}
+      />
+    )
+  }
 }
